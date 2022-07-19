73f84bea928057b46d13aa504dfdd34f7e41be04
anilnani123 <108733458+anilnani123@users.noreply.github.com>
2022-07-19 17:34:20 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,6 +11,11 @@ def newDeploy(jobname,ip,context)
 {
  deploy adapters: [tomcat9(credentialsId: '0f783f78-088a-4576-a3be-24a2481b65dd', path: '', url: 'http://172.31.10.87:8080')], contextPath: 'qaapp', war: '**/*.war'
 }
+
+def runselenium(jobname)
+{
+  sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
+}
   
   
   
