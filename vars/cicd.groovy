f8df71706a493cdf180a092bdfc08984e9cc0e29
anilnani123 <108733458+anilnani123@users.noreply.github.com>
2022-07-19 16:46:01 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,10 +7,11 @@ def newMaven()
 {
   sh 'mvn package'
 }
-def newDeploy()
+def newDeploy(jobname,ip,context)
 {
-  
+ deploy adapters: [tomcat9(credentialsId: '0f783f78-088a-4576-a3be-24a2481b65dd', path: '', url: 'http://172.31.10.87:8080')], contextPath: 'qaapp', war: '**/*.war'
 }
+  
   
   
 
